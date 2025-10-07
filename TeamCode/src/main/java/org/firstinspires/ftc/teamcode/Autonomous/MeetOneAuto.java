695b57066f96c4540f6cc3493f0b86bbfbbebce8
Random-Stuff-Here <randomassetshere1@outlook.com>
2025-10-07 18:44:28 -0500

    First Meet Autonomous
    Features should be complete, but specific steps still need implementing.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/MeetOneAuto.java
@@ -0,0 +1,284 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.qualcomm.hardware.dfrobot.HuskyLens;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
+import com.qualcomm.robotcore.hardware.SwitchableLight;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name="MeetOneAuto", group="Robot")
+public class MeetOneAuto extends LinearOpMode {
+
+    /* Declare OpMode members. */
+    private DcMotor         leftFrontDrive   = null;
+    private DcMotor         rightFrontDrive  = null;
+    private DcMotor         leftBackDrive   = null;
+    private DcMotor         rightBackDrive  = null;
+    private DcMotor         shooter = null;
+    private NormalizedColorSensor colorSensor;
+    private HuskyLens huskyLens;
+
+
+
+    private ElapsedTime     runtime = new ElapsedTime();
+
+    // Calculate the COUNTS_PER_INCH for your specific drive train.
+    // Go to your motor vendor website to determine your motor's COUNTS_PER_MOTOR_REV
+    // For external drive gearing, set DRIVE_GEAR_REDUCTION as needed.
+    // For example, use a value of 2.0 for a 12-tooth spur gear driving a 24-tooth spur gear.
+    // This is gearing DOWN for less speed and more torque.
+    // For gearing UP, use a gear ratio less than 1.0. Note this will affect the direction of wheel rotation.
+    static final double     COUNTS_PER_MOTOR_REV    = 384.5 ;    // eg: TETRIX Motor Encoder
+    static final double     DRIVE_GEAR_REDUCTION    = 1.0 ;     // No External Gearing.
+    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
+    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.141592653589);
+    static final double     DRIVE_SPEED             = 0.6;
+    static final double     TURN_SPEED              = 0.5;
+    public String artifiactPattern;
+
+
+
+    @Override
+    public void runOpMode() {
+
+        // Initialize the drive system variables.
+        leftFrontDrive  = hardwareMap.get(DcMotor.class, "LFMotor");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "RFMotor");
+        leftBackDrive  = hardwareMap.get(DcMotor.class, "LBMotor");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "RBMotor");
+        shooter = hardwareMap.get(DcMotor.class, "shooter");
+        colorSensor = hardwareMap.get(NormalizedColorSensor.class, "sensor_color");
+        huskyLens = hardwareMap.get(HuskyLens.class, "huskylens");
+
+        if (!huskyLens.knock()) {
+            telemetry.addData(">>", "Problem communicating with " + huskyLens.getDeviceName());
+        } else {
+            telemetry.addData(">>", "Press start to continue");
+        }
+
+        huskyLens.selectAlgorithm(HuskyLens.Algorithm.TAG_RECOGNITION);
+
+        // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
+        // When run, this OpMode should start both motors driving forward. So adjust these two lines based on your first test drive.
+        // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
+        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
+        shooter.setDirection(DcMotor.Direction.FORWARD);
+
+        leftFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+
+        leftFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBackDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBackDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        if (colorSensor instanceof SwitchableLight) {
+            ((SwitchableLight)colorSensor).enableLight(true);
+        }
+
+        colorSensor.setGain(1);
+
+
+        // Send telemetry message to indicate successful Encoder reset
+        telemetry.addData("Starting at",  "%7d :%7d",
+                leftFrontDrive.getCurrentPosition(),
+                rightFrontDrive.getCurrentPosition(),
+                leftBackDrive.getCurrentPosition(),
+                rightBackDrive.getCurrentPosition());
+        telemetry.update();
+
+        // Wait for the game to start (driver presses START)
+        waitForStart();
+        HuskyLens.Block block = null;
+        //PPG = 4 GPP = 5 PGP = 1
+        if (block.id == 1) {
+            artifiactPattern = "PGP";
+        }
+        if(block.id == 4) {
+            artifiactPattern = "PPG";
+        }
+        if(block.id == 5) {
+               artifiactPattern = "GPP";
+        }
+
+
+
+
+
+        // Step through each leg of the path,
+        // Note: Reverse movement is obtained by setting a negative distance (not speed)
+        //encoderDrive(DRIVE_SPEED,   72, 72, 72, 72, false, 0, 5);  // S2: Turn Right 12 Inches with 4 Sec timeout
+        //encoderDrive(1, -13.5, 13.5, -13.5, 13.5, false, 0,3);  // S3: Reverse 24 Inches with 4 Sec timeout
+        encoderDrive(0,0,0,0,0, false,true,0.6,5);
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
+        sleep(1000);  // pause to display final telemetry message.
+    }
+
+    /*
+     *  Method to perform a relative move, based on encoder counts.
+     *  Encoders are not reset as the move is based on the current position.
+     *  Move will stop if any of three conditions occur:
+     *  1) Move gets to the desired position
+     *  2) Move runs out of time
+     *  3) Driver stops the OpMode running.
+     */
+    public void encoderDrive(double speed,
+                             double leftFrontInches, double rightFrontInches,
+                             double leftBackInches, double rightBackInches, boolean strafe, boolean Shooting, double shooterPower,
+                             double timeoutS) {
+        int newLeftFrontTarget;
+        int newRightFrontTarget;
+        int newLeftBackTarget;
+        int newRightBackTarget;
+        int shootingStep = 1;
+        String Color = "Empty";
+        NormalizedRGBA colors = colorSensor.getNormalizedColors();
+
+
+        // Ensure that the OpMode is still active
+        if (opModeIsActive()) {
+
+            if (strafe){
+                leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+                rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+                leftBackDrive.setDirection(DcMotor.Direction.FORWARD);
+                rightBackDrive.setDirection(DcMotor.Direction.REVERSE);
+            } else {
+                leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+                rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+                leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
+                rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
+            }
+
+            // Determine new target position, and pass to motor controller
+            newLeftFrontTarget = leftFrontDrive.getCurrentPosition() + (int)(leftFrontInches * COUNTS_PER_INCH);
+            newRightFrontTarget = rightFrontDrive.getCurrentPosition() + (int)(rightFrontInches * COUNTS_PER_INCH);
+            newLeftBackTarget = leftBackDrive.getCurrentPosition() + (int)(leftBackInches * COUNTS_PER_INCH);
+            newRightBackTarget = rightBackDrive.getCurrentPosition() + (int)(rightBackInches * COUNTS_PER_INCH);
+            leftFrontDrive.setTargetPosition(newLeftFrontTarget);
+            rightFrontDrive.setTargetPosition(newRightFrontTarget);
+            leftBackDrive.setTargetPosition(newLeftBackTarget);
+            rightBackDrive.setTargetPosition(newRightBackTarget);
+
+            // Turn On RUN_TO_POSITION
+            leftFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rightFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            leftBackDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rightBackDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // reset the timeout time and start motion.
+            runtime.reset();
+            leftFrontDrive.setPower(Math.abs(speed));
+            rightFrontDrive.setPower(Math.abs(speed));
+            leftBackDrive.setPower(Math.abs(speed));
+            rightBackDrive.setPower(Math.abs(speed));
+
+
+
+            // keep looping while we are still active, and there is time left, and both motors are running.
+            // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
+            // its target position, the motion will stop.  This is "safer" in the event that the robot will
+            // always end the motion as soon as possible.
+            // However, if you require that BOTH motors have finished their moves before the robot continues
+            // onto the next step, use (isBusy() || isBusy()) in the loop test.
+            while (opModeIsActive() &&
+                    (runtime.seconds() < timeoutS) &&
+                    (leftFrontDrive.isBusy() && rightFrontDrive.isBusy() && leftBackDrive.isBusy() && rightBackDrive.isBusy())) {
+
+                if (colors.green > colors.blue) {
+                    Color = "Green";
+                } else {
+                    Color = "Purple";
+                }
+
+                if(Shooting) {
+                    if (artifiactPattern == "PGP"){
+                        if(shootingStep == 1 && Color == "Purple"){
+                            shooter.setPower(shooterPower);
+                            shootingStep = shootingStep + 1;
+                        }
+                        if(shootingStep == 2 && Color == "Green"){
+                            shooter.setPower(shooterPower);
+                            shootingStep = shootingStep + 1;
+                        }
+                        if(shootingStep == 3 && Color == "Purple"){
+                            shooter.setPower(shooterPower);
+                            shootingStep = 1;
+                        }
+                    }
+                    if (artifiactPattern == "PPG"){
+                        if(shootingStep == 1 && Color == "Purple"){
+                            shooter.setPower(shooterPower);
+                            shootingStep = shootingStep + 1;
+                        }
+                        if(shootingStep == 2 && Color == "Purple"){
+                            shooter.setPower(shooterPower);
+                            shootingStep = shootingStep + 1;
+                        }
+                        if(shootingStep == 3 && Color == "Green"){
+                            shooter.setPower(shooterPower);
+                            shootingStep = 1;
+                        }
+                    }
+                    if (artifiactPattern == "GPP"){
+                        if(shootingStep == 1 && Color == "Green"){
+                            shooter.setPower(shooterPower);
+                            shootingStep = shootingStep + 1;
+                        }
+                        if(shootingStep == 2 && Color == "Purple"){
+                            shooter.setPower(shooterPower);
+                            shootingStep = shootingStep + 1;
+                        }
+                        if(shootingStep == 3 && Color == "Purple"){
+                            shooter.setPower(shooterPower);
+                            shootingStep = 1;
+                        }
+                    }
+                }
+
+
+
+                // Display it for the driver.
+                telemetry.addData("Running to",  " %7d :%7d", newLeftFrontTarget,  newRightFrontTarget, newLeftBackTarget,  newRightBackTarget);
+                telemetry.addData("Currently at",  " at %7d :%7d",
+                        leftFrontDrive.getCurrentPosition(), rightFrontDrive.getCurrentPosition(), leftBackDrive.getCurrentPosition(), rightBackDrive.getCurrentPosition());
+                telemetry.addLine()
+                        .addData("Color:", Color);
+                telemetry.addLine()
+                        .addData("Red", colors.red)
+                        .addData("Green", colors.green)
+                        .addData("Blue", colors.blue);
+                telemetry.update();
+            }
+
+            // Stop all motion;
+            leftFrontDrive.setPower(0);
+            rightFrontDrive.setPower(0);
+            leftBackDrive.setPower(0);
+            rightBackDrive.setPower(0);
+            shooter.setPower(0);
+
+
+            // Turn off RUN_TO_POSITION
+            leftFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            rightFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            leftBackDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            rightBackDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+            sleep(250);   // optional pause after each move.
+        }
+    }
+}
+
