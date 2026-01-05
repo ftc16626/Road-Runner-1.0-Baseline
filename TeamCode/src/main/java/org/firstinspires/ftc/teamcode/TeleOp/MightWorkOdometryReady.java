a0771089735711338283ac9c301f3c333d140790
Ieatdogs993 <perryhembree@gmail.com>
2026-01-05 17:10:01 -0600

    aaaa

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MightWorkOdometryReady.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MightWorkOdometryReady.java
@@ -35,7 +35,7 @@ import java.util.concurrent.TimeUnit;
  * All current PID, servo, and button mappings preserved.
  * (Is this language simple enough?)
  */
-@TeleOp(name = "mightworkOdometryReady", group = "robot")
+@TeleOp(name = "Tele-Op_Meet_4", group = "robot")
 
 public class MightWorkOdometryReady extends LinearOpMode {
 
